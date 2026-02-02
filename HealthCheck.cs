b124147365539c142e91c795cfa6d7b182f58424
Oliver Tattersfield <ot153954@renishaw.com>
2026-02-02 11:29:44 +0000

    Added test for TranscriptAPI to save on credits

---
+++ HealthCheck.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace Company.Function;
+
+public class HealthCheck
+{
+    private readonly ILogger<HealthCheck> _logger;
+
+    public HealthCheck(ILogger<HealthCheck> logger)
+    {
+        _logger = logger;
+    }
+
+    [Function("HealthCheck")]
+    public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+    {
+        _logger.LogInformation("API is running");
+        return new OkObjectResult("API is running\n");
+    }
+}
